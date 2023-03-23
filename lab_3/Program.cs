33510d6db7a1fdd79007864414fd2cf8279077fb
Kristina <kristinaer304@gmail.com>
2023-03-23 22:04:24 +0200

    dictionary explanation upd

--- lab_3/Program.cs
+++ lab_3/Program.cs
@@ -2,18 +2,12 @@
 
 var dictionary = new StringsDictionary();
 
-// перевірити як працює хешування, потім видалемо 
-dictionary.Add("Hello", "Hi");
-dictionary.Add("Goodbye", "Bye");
-Console.WriteLine(dictionary.Get("Hello"));
-Console.WriteLine(dictionary.Get("Goodbye"));
-
-var lines = File.ReadAllLines("dictionary.txt");
-foreach (var line in lines)
+foreach (var line in File.ReadAllLines("dictionary.txt"))
 {
     string[] elements = line.Split("; ");
     var key = elements[0];
     var value = String.Join("; ", elements[1..]);
+    dictionary.Add(key, value);
 }
 
 while (true)
@@ -21,8 +15,7 @@ while (true)
     Console.WriteLine("\nEnter a word:");
     var word = Console.ReadLine();
     string? explanation = default;
-
-    // get explanation from the dictionary;
     
-    Console.WriteLine($"Explanation: {explanation}");
+    explanation = dictionary.Get(word);
+    Console.WriteLine($"\nExplanation:\n{explanation}");
 }
