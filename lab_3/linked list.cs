252f6d53ffe8aa2a8492e5c8ae7a5ce8931fcd9b
Kristina <kristinaer304@gmail.com>
2023-03-24 16:40:03 +0200

    Linked list upd

--- lab_3/linked list.cs
+++ lab_3/linked list.cs
@@ -1,96 +1,96 @@
 namespace lab_3;
 
+public class KeyValuePair
+{
+    public string Key { get; }
+
+    public string Value { get; }
+
+    public KeyValuePair(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
+
+public class LinkedListNode
+{
+    public KeyValuePair Pair { get; }
+        
+    public LinkedListNode Next { get; set; }
+
+    public LinkedListNode(KeyValuePair pair, LinkedListNode next = null)
+    {
+        Pair = pair;
+        Next = next;
+    }
+}
+
 public class linked_list
 {
-    public class KeyValuePair
+
+    private LinkedListNode _first;
+    private LinkedListNode _last;
+    private int _pointer;
+
+    public void Add(KeyValuePair pair)
     {
-        public string Key { get; }
-
-        public string Value { get; }
-
-        public KeyValuePair(string key, string value)
+        LinkedListNode node = new LinkedListNode(pair);
+        if (_first == null)
         {
-            Key = key;
-            Value = value;
+            _first = node;
+            _last = node;
+        }
+        else
+        {
+            _last.Next = node;
+            _last = node;
         }
     }
 
-    public class LinkedListNode
+    public void RemoveByKey(string key)
     {
-        public KeyValuePair Pair { get; }
+        LinkedListNode current = _first;
+        LinkedListNode previous = null;
         
-        public LinkedListNode Next { get; set; }
-
-        public LinkedListNode(KeyValuePair pair, LinkedListNode next = null)
+        while (current != null)
         {
-            Pair = pair;
-            Next = next;
+            if (current.Pair.Key == key)
+            {
+                if (previous == null)
+                {
+                    _first = current.Next; //видалення першого вузла
+                }
+                else
+                {
+                    previous.Next = current.Next;// видалення не першого вузла
+                }
+
+                if (current == _last)
+                {
+                    _last = previous; //оновити посилання на останній вузол, якщо необхідно
+                }
+
+                break; // вихід із циклу після першого знайденого збігу
+            }
+            previous = current;
+            current = current.Next;// перехід до наступного вузла
         }
     }
-    public class LinkedList
+
+    public KeyValuePair? GetItemWithKey(string key)
     {
-        private LinkedListNode _first;
-        private LinkedListNode _last;
-
-        public void Add(KeyValuePair pair)
+        LinkedListNode current = _first;
+        while (current!=null)
         {
-            LinkedListNode node = new LinkedListNode(pair);
-            if (_first == null)
+            if (current.Pair.Key == key)
             {
-                _first = node;
-                _last = node;
+                return current.Pair;// повертає перший знайдений збіг
             }
-            else
-            {
-                _last.Next = node;
-                _last = node;
-            }
+            current = current.Next;
         }
 
-        public void RemoveByKey(string key)
-        {
-            LinkedListNode current = _first;
-            LinkedListNode previous = null;
-            
-            while (current != null)
-            {
-                if (current.Pair.Key == key)
-                {
-                    if (previous == null)
-                    {
-                        _first = current.Next; //видалення першого вузла
-
-                    }
-                    else
-                    {
-                        previous.Next = current.Next;// видалення не першого вузла
-                    }
-
-                    if (current == _last)
-                    {
-                        _last = previous; //оновити посилання на останній вузол, якщо необхідно
-                    }
-
-                    break; // вихід із циклу після першого знайденого збігу
-                }
-                previous = current;
-                current = current.Next;// перехід до наступного вузла
-            }
-        }
-
-        public KeyValuePair GetItemWithKey(string key)
-        {
-            LinkedListNode current = _first;
-            while (current!=null)
-            {
-                if (current.Pair.Key == key)
-                {
-                    return current.Pair;// повертає перший знайдений збіг
-                }
-                current = current.Next;
-            }
-
-            return null;
-        }
+        return null;
     }
+  
 }
